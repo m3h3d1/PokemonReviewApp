c205ac884d29e57384480fe3655e244d72f307ff
m3h3d1 <hasanmehedi185@gmail.com>
2023-06-28 20:51:21 +0600

    Adding Repository

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Intefaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository: IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
