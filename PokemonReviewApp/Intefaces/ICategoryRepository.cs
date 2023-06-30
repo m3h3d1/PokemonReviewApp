0eb379f6828e29d7c8dc460983efef6cae17a372
m3h3d1 <hasanmehedi185@gmail.com>
2023-06-30 13:26:45 +0600

    Get & Read Methods - Part 2 - Category

---
+++ PokemonReviewApp/Intefaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Intefaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+    }
+}
